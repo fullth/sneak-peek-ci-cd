885fc7b2c0190e9e14f986cdaed32fdb9ae35449
fullth <lth129@naver.com>
2022-08-02 20:33:29 +0900

    test github actions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello docker!';
+    return 'Hello github actions!';
   }
 }
